b1ab06eed03e87cb8957ba2a1ac9d8f585ad1ea5
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2020-07-15 11:48:58 +0700

    addtestTriangle

--- src/BT_triangle/TestTriangle.java
+++ src/BT_triangle/TestTriangle.java
@@ -12,14 +12,6 @@ public class TestTriangle {
         System.out.println("Enter edge C : ");
         int edgeC = sc.nextInt();
 
-        Triangle triangle = new Triangle();
-        if(triangle.compareTriangle(edgeA, edgeB, edgeC) == 0){
-            System.out.println("Not a triangle!");
-        }else if(triangle.compareTriangle(edgeA, edgeB, edgeC) == 1){
-            System.out.println("A normal triangle");
-        }else if(triangle.compareTriangle(edgeA, edgeB, edgeC) == 2){
-            System.out.println("A isosceles triangle");
-        }else
-            System.out.println("A equilateral triangle");
+        System.out.println(Triangle.compareTriangle(edgeA, edgeB, edgeC));
     }
 }
--- src/BT_triangle/Triangle.java
+++ src/BT_triangle/Triangle.java
@@ -1,27 +1,27 @@
 package BT_triangle;
 
 public class Triangle {
-    private int edgeA;
-    private int edgeB;
-    private int edgeC;
+    private static int edgeA;
+    private static int edgeB;
+    private static int edgeC;
 
     public Triangle() {
     }
 
-    public int compareTriangle(int edgeA, int edgeB, int edgeC){
+    public static String compareTriangle(int edgeA, int edgeB, int edgeC){
         if(edgeA <= 0 || edgeB <= 0 || edgeC <= 0 ||
                 Math.abs(edgeA) > (Math.abs(edgeB)+Math.abs(edgeC)) ||
                 Math.abs(edgeB) > (Math.abs(edgeA)+Math.abs(edgeC)) ||
                 Math.abs(edgeC) > (Math.abs(edgeB)+Math.abs(edgeA))){
-            return 0;
+            return "Not a triangle!";
         }else if(edgeA == edgeB){
             if(edgeB == edgeC){
-                return 3;
+                return "A equilateral triangle";
             }else
-                return 2;
-        }else if(edgeB == edgeC || edgeA == edgeC){
-            return 2;
+                return "A isosceles triangle";
+        }else if(edgeB == edgeC || edgeA == edgeC) {
+            return "A isosceles triangle";
         }else
-            return 1;
+            return "A normal triangle";
     }
 }
